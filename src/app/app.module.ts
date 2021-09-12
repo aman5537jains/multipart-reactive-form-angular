14bd6ba9f9208865cab30acc04c35f00a8d403ae
aman5537jains <aman5537jains@gmail.com>
2021-09-12 11:18:37 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { UploadFileComponent } from './upload-file/upload-file.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [AppComponent, UploadFileComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/upload-file/upload-file.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {  HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.css']
+})
+export class UploadFileComponent implements OnInit {
+  form: FormGroup = new FormGroup(
+    {
+      name:new FormControl("",[Validators.required,Validators.minLength(4)]),
+      image:new FormControl("",[Validators.required,Validators.email]),
+       
+      
+    }
+    
+  );
+  constructor(public http:HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  onChange(e:any){
+    let extensionAllowed:any = { "png": true, "jpeg": true };
+    let file =  e.target.files[0] ;
+
+     
+      if (file.size / 1024 / 1024 > 20) {
+        alert("File size should be less than 20MB")
+        return;
+      }
+      if (extensionAllowed) {
+        var nam = file.name.split('.').pop();
+        if (!extensionAllowed[nam]) {
+          alert("Please upload " + Object.keys(extensionAllowed) + " file.")
+          return;
+        }
+      }
+     
+    
+   
+   
+    this.form.controls["image"].setValue(file);
+  }
+  uplaodFile(){
+    let form = this.form.value;
+    const uploadData = new FormData();
+    for (let input_name in form) {
+      if (form[input_name] instanceof Blob) // check is file
+      {
+       
+        uploadData.append(input_name, form[input_name], form[input_name].name ? form[input_name].name : "");
+      
+      }
+        else
+        uploadData.append(input_name, form[input_name]);
+    }
+    
+    return this.http.post("http://localhost/info.php", uploadData).subscribe(data=>{
+      alert(JSON.stringify(data));
+    })
+      
+       
+      
+  }
+
+}
